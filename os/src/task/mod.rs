928276cb773a9001e2abf725e266cd6df0c6ee97
CentaureaQ <FyeeQu@outlook.com>
2024-01-10 10:36:05 +0800

    try for task3

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -14,7 +14,10 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
-use crate::config::MAX_APP_NUM;
+//use crate::config::MAX_APP_NUM;
+// modify
+use crate::config::{MAX_APP_NUM, MAX_SYSCALL_NUM};
+// modify end
 use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
 use lazy_static::*;
@@ -54,6 +57,9 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            // modify the array to store the sys call times
+            sys_call_times:[0;MAX_SYSCALL_NUM],
+            // modify end
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -135,6 +141,18 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+    // modify 
+    fn increase_sys_call(&self, sys_id: usize) {
+        let mut inner: core::cell::RefMut<'_, TaskManagerInner> = self.inner.exclusive_access();
+        let current_task = inner.current_task;
+        inner.tasks[current_task].sys_call_times[sys_id] += 1;
+    }
+
+    fn get_sys_call_times(&self) -> [u32; MAX_SYSCALL_NUM] {
+        let inner: core::cell::RefMut<'_, TaskManagerInner> = self.inner.exclusive_access();
+        inner.tasks[inner.current_task].sys_call_times.clone()
+    }
+    // modify end
 }
 
 /// Run the first task in task list.
@@ -169,3 +187,15 @@ pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
 }
+
+// modify
+/// Increase the sys call count
+pub fn increase_sys_call(sys_id: usize) {
+    TASK_MANAGER.increase_sys_call(sys_id);
+}
+
+/// return the sys count array of the current task
+pub fn get_sys_call_times() -> [u32; MAX_SYSCALL_NUM] {
+    TASK_MANAGER.get_sys_call_times()
+}
+// modify end
